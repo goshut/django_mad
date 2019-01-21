a8135129032024dde3ecab1eeecf68d414a4a62c
goshut <952918451@qq.com>
2019-01-21 21:59:42 +0800

    登陆

--- meiduo_mall/apps/users/urls.py
+++ meiduo_mall/apps/users/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from rest_framework_jwt.views import obtain_jwt_token
 
 from . import views
 
@@ -6,4 +7,6 @@ urlpatterns = [
     url(r'^users/$', views.UserView.as_view()),
     url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
     url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
+    # JWT登录
+    url(r'^authorizations/$', obtain_jwt_token),
 ]
