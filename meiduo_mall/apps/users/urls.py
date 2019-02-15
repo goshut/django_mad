34bdf205e33bfae6d01b73c86a6d0945b582f9c9
goshut <952918451@qq.com>
2019-02-15 20:34:12 +0800

    地址相关

--- meiduo_mall/apps/users/urls.py
+++ meiduo_mall/apps/users/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token
 
 from . import views
@@ -12,3 +13,14 @@ urlpatterns = [
     url(r'^emails/verification/$', views.VerifyEmailView.as_view()),  # 邮箱验证
     url(r'^authorizations/$', obtain_jwt_token),  # JWT登录
 ]
+
+router = routers.DefaultRouter()
+router.register(r'addresses', views.AddressViewSet, base_name='addresses')
+
+urlpatterns += router.urls
+# POST /addresses/ 新建  -> create
+# PUT /addresses/<pk>/ 修改  -> update
+# GET /addresses/  查询  -> list
+# DELETE /addresses/<pk>/  删除 -> destroy
+# PUT /addresses/<pk>/status/ 设置默认 -> status
+# PUT /addresses/<pk>/title/  设置标题 -> title
--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -1,9 +1,13 @@
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView
+from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
 
+from users import constants
 from users.models import User
 from . import serializers
 
@@ -92,3 +96,69 @@ class VerifyEmailView(APIView):
             user.save()
             return Response({'message': 'OK'})
 
+
+class AddressViewSet(CreateModelMixin, UpdateModelMixin, GenericViewSet):
+    """
+    用户地址新增与修改
+    """
+    serializer_class = serializers.UserAddressSerializer
+    permissions = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.request.user.addresses.filter(is_deleted=False)
+
+    # GET /addresses/
+    def list(self, request, *args, **kwargs):
+        """
+        用户地址列表数据
+        """
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        user = self.request.user
+        return Response({
+            'user_id': user.id,
+            'default_address_id': user.default_address_id,
+            'limit': constants.USER_ADDRESS_COUNTS_LIMIT,
+            'addresses': serializer.data,
+        })
+
+    def create(self, request, *args, **kwargs):
+        """
+        保存用户地址数据
+        """
+        # 检查用户地址数据数目不能超过上限
+        count = request.user.addresses.count()
+        if count > constants.USER_ADDRESS_COUNTS_LIMIT:
+            return Response({'message': '保存地址数据已达到上限'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return super().create(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        address = self.get_object()
+        address.is_deleted = True
+        address.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # put /addresses/pk/status/
+    @action(methods=['put'], detail=True)
+    def status(self, request, pk=None):
+        """
+        设置默认地址
+        """
+        address = self.get_object()
+        request.user.default_address = address
+        request.user.save()
+        return Response({'message': 'OK'}, status=status.HTTP_200_OK)
+
+    # put /addresses/pk/title/
+    # 需要请求体参数 title
+    @action(methods=['put'], detail=True)
+    def title(self, request, pk=None):
+        """
+        修改标题
+        """
+        address = self.get_object()
+        serializer = serializers.AddressTitleSerializer(instance=address, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
