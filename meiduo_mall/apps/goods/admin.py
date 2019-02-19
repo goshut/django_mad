55bb03110b527b56ba950674d3c73ed8821c97c9
goshut <952918451@qq.com>
2019-02-20 01:07:10 +0800

    检测修改admin里的商品类目时自动生成list.html文件

--- meiduo_mall/apps/goods/admin.py
+++ meiduo_mall/apps/goods/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 
+from goods.models import GoodsChannel
 from . import models
+
 # Register your models here.
-admin.site.register(models.GoodsCategory)
+
+
+class GoodsCategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        from celery_tasks.html.tasks import generate_static_list_search_html
+        generate_static_list_search_html.delay()
+
+    def delete_model(self, request, obj):
+        obj.delete()
+        from celery_tasks.html.tasks import generate_static_list_search_html
+        generate_static_list_search_html.delay()
+
+
+admin.site.register(models.GoodsCategory, GoodsCategoryAdmin)
 admin.site.register(models.GoodsChannel)
 admin.site.register(models.Goods)
 admin.site.register(models.Brand)
@@ -11,3 +29,5 @@ admin.site.register(models.SpecificationOption)
 admin.site.register(models.SKU)
 admin.site.register(models.SKUSpecification)
 admin.site.register(models.SKUImage)
+
+
