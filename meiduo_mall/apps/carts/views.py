5b31b40090ecc54399355fce4ddfa0c040bef39a
goshut <952918451@qq.com>
2019-02-25 16:45:04 +0800

    购物车模块注册2添加功能模块文件

---
+++ meiduo_mall/apps/carts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+
+class CartView(APIView):
+
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
